a32ee45879a884ee93c78eb258a5ea9a0c499e30
mayie18110-star <mayie18110@gmail.com>
2026-03-04 11:41:24 -0500

    nuevo java

--- script.js
+++ script.js
@@ -3,9 +3,27 @@ function agregarTarea() {
     const lista = document.getElementById("listaTareas");
 
     if (input.value.trim() !== "") {
-        const nuevaTarea = document.createElement("li");
-        nuevaTarea.textContent = input.value;
-        lista.appendChild(nuevaTarea);
+
+        const li = document.createElement("li");
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+
+        const span = document.createElement("span");
+        span.textContent = input.value;
+
+        checkbox.addEventListener("change", function() {
+            if (checkbox.checked) {
+                span.classList.add("completada");
+            } else {
+                span.classList.remove("completada");
+            }
+        });
+
+        li.appendChild(checkbox);
+        li.appendChild(span);
+        lista.appendChild(li);
+
         input.value = "";
     }
 }
